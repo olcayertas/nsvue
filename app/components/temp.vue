694922e70e656bbece4cf8ff26c73423563e2cf3
Olcay Ertaş <olcayertas@gmail.com>
2022-09-14 19:32:01 +0300

    implemented area chart

---
+++ app/components/temp.vue
@@ -0,0 +1,20 @@
+<template>
+    <Page actionBarHidden="true">
+        <StackLayout class="root">
+            ...
+            <FlexboxLayout class="bottom" flexDirection="column">
+                ...
+                <RadCartesianChart class="chart" ref="chart">
+                    <LinearAxis v-tkCartesianHorizontalAxis hidden="true"/>
+                    <LinearAxis v-tkCartesianVerticalAxis hidden="true"/>
+                    <AreaSeries
+                        :items="items"
+                        valueProperty="value"
+                        categoryProperty="date"
+                        v-tkCartesianSeries
+                    />
+                </RadCartesianChart>
+            </FlexboxLayout>
+        </StackLayout>
+    </Page>
+</template>
